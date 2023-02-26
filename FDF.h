20c35a24ec53517a74a8179ce0c390e2e29dd886
Mohcine-Ghalmi <mohcin.ghalmi@gmail.com>
2023-02-26 17:39:38 +0100

    commit

--- FDF.h
+++ FDF.h
@@ -6,7 +6,7 @@
 /*   By: mghalmi <mghalmi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 15:42:47 by mghalmi           #+#    #+#             */
-/*   Updated: 2023/02/26 16:42:25 by mghalmi          ###   ########.fr       */
+/*   Updated: 2023/02/26 17:32:27 by mghalmi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,50 @@
 # include <ctype.h>
 # include "get_next_line/get_next_line.h"
 # include "libft/libft.h"
+
+# define LEFT	65361
+# define UP		65362
+# define RIGHT	65363
+# define DOWN	65364
+
+# define NUMPAD_1	65436
+# define NUMPAD_2	65433
+# define NUMPAD_3	65435
+# define NUMPAD_4	65430
+# define NUMPAD_5	65437
+# define NUMPAD_6	65432
+# define NUMPAD_7	65429
+# define NUMPAD_8	65431
+# define NUMPAD_9	65434
+
+# define NUM_1	49
+# define NUM_2	50
+# define NUM_3	51
+# define NUM_4	52
+# define NUM_5	53
+# define NUM_6	54
+# define NUM_7	55
+# define NUM_8	56
+# define NUM_9	57
+
+# define PLUS	61
+# define MINUS	45
+# define SPACE	32
+
+# define Z		122
+# define X		120
+# define P		112
+# define I		105
+# define O		111
+# define L		108
+# define R		114
+# define A		97
+# define S		115
+# define D		100
+# define Q		113
+# define W		119
+# define E		101
+# define ESC    65307
 
 typedef struct point
 {
